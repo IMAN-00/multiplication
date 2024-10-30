c4a35c0b662ed4344a58577a0bb23e9dc86b2aff
IMAN-00 <imanalawneh928@gmail.com>
2024-10-30 20:50:07 +0200

    first commit

---
+++ Assets/MULTI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MULTI : MonoBehaviour
+{
+
+    void Start()
+    {
+    
+       for (int i = 5; i ==5 ; i++)
+       for (int j = 1; j <11 ; j++)
+       {
+            Debug.Log (i * j);
+            Debug.Log ();
+       }
+       
+    }
+    
+        
+  
+}
